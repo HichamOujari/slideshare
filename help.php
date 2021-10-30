ff0b2ef50c09486779a30d429ddde64b3d4341a5
hichamoujari <hichamoujari99@gmail.com>
2021-10-30 17:42:32 +0200

    by hicham

---
+++ help.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        ul{
+            list-style: none;
+        }
+        ul>li>p{
+            font-size: 20px;
+            font-weight: bold;
+        }
+        .imgpluspetit{
+            width: 100%;
+            height:70vh;
+        }
+    </style>
+</head>
+<body>
+    <ul>
+        <li>
+            <p>1) - Go to : <a href="https://www.slideshare.net/">https://www.slideshare.net/</a></p>
+            <img class="imgpluspetit" src="./assets/steps/1.jpg" />
+        </li>
+        <li>
+            <p>2) - Choose an article</p>
+            <img class="imgpluspetit"  src="./assets/steps/2.jpg" />
+        </li>
+        <li>
+            <p>3) - Copie the link</p>
+            <img src="./assets/steps/3.jpg" />
+        </li>
+        <li>
+            <p>4) - Paste it here and congratulations</p>
+            <img src="./assets/steps/4.jpg" />
+        </li>
+        
+    </ul>
+</body>
+</html>
--- index.php
+++ index.php
@@ -19,29 +19,71 @@ if (isset($_POST["submitform"])) {
                 justify-content: center;
             }
 
-            img {
+            .imgpdf {
                 width: 100vw;
                 height: 100vh;
+            }
+
+            .loading {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color: rgba(0, 0, 0, 0.8);
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center
+            }
+
+            .loading>img {
+                width: 40%;
+                min-width: 400px;
             }
         </style>
     </head>
 
     <body>
+
+        <div id="loadingshow" class="loading">
+            <img src="./assets/loading.gif" />
+        </div>
         <script>
+            var afterPrint = function() {
+                document.location.href=document.location.href;
+            };
+
+            if (window.matchMedia) {
+                var mediaQueryList = window.matchMedia('print');
+                mediaQueryList.addListener(function(mql) {
+                    if (!mql.matches){
+                        afterPrint();
+                    }
+                });
+            }
+
+            window.onafterprint = afterPrint;
+
             function download() {
                 var path = document.getElementsByClassName("slide_image")[0]
+                var n = document.getElementsByClassName("slide_image").length
                 if (path == undefined) {
                     console.log(document.getElementsByClassName("notranslate")[1].href);
+                } else {
+                    path = path.src;
+                    document.body.innerHTML = "";
+                    for (var i = 1; i <= n; i++) {
+                        var src = path.split("-1-")[0] + "-" + i + "-" + path.split("-1-")[1]
+                        var tag = document.createElement('img');
+                        tag.src = src;
+                        document.body.appendChild(tag);
+                    }
+                    var rsp = window.print()
+
+
+
                 }
-                path = path.src;
-                document.body.innerHTML = "";
-                for (var i = 1; i < 55; i++) {
-                    var src = path.split("-1-")[0] + "-" + i + "-" + path.split("-1-")[1]
-                    var tag = document.createElement('img');
-                    tag.src = src;
-                    document.body.appendChild(tag);
-                }
-                window.print();
             }
 
             download();
@@ -66,11 +108,22 @@ if (isset($_POST["submitform"])) {
     </head>
 
     <body>
-        <img src="./assets/logo.jpg" />
-        <form method="post">
+        <a class="howtouse" href="./help.php">How use it ?</a>
+        <img id="idlogo" src="./assets/logo.jpg" />
+        <form id="idform" onsubmit="loading()" method="post">
             <input type="text" placeholder="Paste here your URL" required name="siteweb" autocomplete="off" />
-            <input type="submit"  name="submitform" value="Download" />
+            <input type="submit" name="submitform" value="Download" />
         </form>
+        <div id="loadingshow" class="loading">
+            <img src="./assets/loading.gif" />
+        </div>
+        <script>
+            function loading() {
+                document.getElementById("idlogo").style.display = "none";
+                document.getElementById("idform").style.display = "none";
+                document.getElementById("loadingshow").style.display = "flex";
+            }
+        </script>
     </body>
 
     </html>
