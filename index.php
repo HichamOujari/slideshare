62a3d3eb94b402e3ae639a24477c03cba8d2e156
hichamoujari <hichamoujari99@gmail.com>
2021-10-30 18:34:26 +0200

    by hihcalm

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST["submitform"])) {
     $html = file_get_contents($_POST['siteweb']);
-    
+
 ?>
     <!DOCTYPE html>
     <html lang="en">
@@ -21,7 +21,7 @@ if (isset($_POST["submitform"])) {
 
             .imgpdf {
                 width: 2480px;
-                height:3508px;
+                height: 3508px;
             }
 
             .loading {
@@ -51,13 +51,13 @@ if (isset($_POST["submitform"])) {
         <?php echo $html; ?>
         <script>
             var afterPrint = function() {
-                document.location.href=document.location.href;
+                document.location.href = document.location.href;
             };
 
             if (window.matchMedia) {
                 var mediaQueryList = window.matchMedia('print');
                 mediaQueryList.addListener(function(mql) {
-                    if (!mql.matches){
+                    if (!mql.matches) {
                         afterPrint();
                     }
                 });
@@ -69,7 +69,7 @@ if (isset($_POST["submitform"])) {
                 var path = document.getElementsByClassName("slide_image")[0]
                 var n = document.getElementsByClassName("slide_image").length
                 if (path == undefined) {
-                    console.log(document.getElementsByClassName("notranslate")[1].href);
+                    document.location.href ="https://slide-share.herokuapp.com/help.php"
                 } else {
                     path = path.src;
                     document.body.innerHTML = '<div id="loadingshow" class="loading"><img src="./assets/loading.gif" /></div>';
@@ -77,6 +77,7 @@ if (isset($_POST["submitform"])) {
                         var src = path.split("-1-")[0] + "-" + i + "-" + path.split("-1-")[1]
                         var tag = document.createElement('img');
                         tag.src = src;
+                        tag.setAttribute('style', "width:210mm;height:291mm;")
                         document.body.appendChild(tag);
                     }
                     document.getElementById("loadingshow").style.display = "none";
